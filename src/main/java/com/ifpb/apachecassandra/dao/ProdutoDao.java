9ede2d3ba47580cc8560a200e6f49606c59882c1
Paulo Freitas <paulo.freitas.nt@gmail.com>
2019-12-10 14:15:59 -0300

    Adicionando outras anotações ao mapper

--- src/main/java/com/ifpb/apachecassandra/dao/ProdutoDao.java
+++ src/main/java/com/ifpb/apachecassandra/dao/ProdutoDao.java
@@ -1,6 +1,7 @@
 package com.ifpb.apachecassandra.dao;
 
 import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.Delete;
 import com.datastax.oss.driver.api.mapper.annotations.Insert;
 import com.datastax.oss.driver.api.mapper.annotations.Select;
 import com.ifpb.apachecassandra.modelo.Produto;
@@ -8,10 +9,16 @@ import com.ifpb.apachecassandra.modelo.Produto;
 @Dao
 public interface ProdutoDao {
 
-    @Insert
-    void salvar(Produto produto);
+    @Insert(ifNotExists = true)
+    boolean salvar(Produto produto);
 
     @Select
     Produto buscarPorCodigo(int codigo);
+
+    @Delete
+    void deletar(Produto produto);
+
+    @Delete(ifExists = true, entityClass = Produto.class)
+    boolean deletarPorCodigo(int codigo);
 
 }
--- src/main/java/com/ifpb/apachecassandra/visao/App.java
+++ src/main/java/com/ifpb/apachecassandra/visao/App.java
@@ -1,18 +1,39 @@
 package com.ifpb.apachecassandra.visao;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import com.ifpb.apachecassandra.dao.ProdutoDao;
 import com.ifpb.apachecassandra.dao.ProdutoDaoStatements;
+import com.ifpb.apachecassandra.mapper.AulaMapper;
+import com.ifpb.apachecassandra.mapper.AulaMapperBuilder;
+import com.ifpb.apachecassandra.modelo.Produto;
 
 public class App {
 
     public static void main(String[] args) {
 
-        ProdutoDaoStatements produtoDao = new ProdutoDaoStatements();
-
+//        ProdutoDaoStatements produtoDao = new ProdutoDaoStatements();
 //        System.out.println(produtoDao
 //            .salvar(new Produto(1,"Arroz Parbolizado",
 //                    3.5f)));
+//        System.out.println(produtoDao.buscarPorCodigo(0));
 
-        System.out.println(produtoDao.buscarPorCodigo(0));
+        try(CqlSession session = new CqlSessionBuilder()
+                .withKeyspace("aula").build()){
+
+            AulaMapper mapper = new AulaMapperBuilder(session)
+                    .build();
+            ProdutoDao dao = mapper.produtoDao();
+
+//            System.out.println(
+//                dao
+//                .salvar(new Produto(5,"Feijão",6)));
+
+//            System.out.println(dao.buscarPorCodigo(5));
+
+            System.out.println(dao.deletarPorCodigo(5));
+
+        }
 
 
     }
